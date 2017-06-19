ca8a2a46924167aa80b3429ada67cb585005abb8
Jederson_Luz <jedersonalpha@gmail.com>
2017-06-19 13:17:29 -0300

    Comentários adicionados ao código

--- yopet.c
+++ yopet.c
@@ -1,7 +1,11 @@
+/*
+ *Autores: Hélio Rocha, Jederson Luz, Myllena Caetano
+ *Emails: junior0helio15@gmail.com, jedersonalpha@gmail.com, myllenah2o@gmail.com
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-  
+
   return 0;
 }
